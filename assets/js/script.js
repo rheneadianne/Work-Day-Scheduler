65673faf9f660d1cf1c7121c8430450f40c58a81
rhenea <rheneadianne@gmail.com>
2022-06-07 23:27:24 -0400

    realized my whole first function can be one line with moment.js and now im upset

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,22 +4,8 @@ let daySuffix = "" // empty string to be defined later
 let date = today.getDate() + daySuffix // current date with suffix (1st, 2nd, 3rd)
 
 const dateAndTime = () => {
-    date === 1 || date === 21 || date === 31? // if certain days, set suffix to st, nd, rd, th
-        daySuffix = "st":
-    date === 2 || date === 22?
-        daySuffix = "nd":
-    date === 3 || date === 23?
-        daySuffix = "rd":
-    daySuffix = "th";
-
-    const weekdayList = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]; //days of weeks in array
-    let dayOfWeek = weekdayList[today.getDay()]; // gets day of week depending on date
-    const monthList = ["January","February","March","April","May","June","July","August","September","October","November","December"]; // name of months in array
-    let month = monthList[today.getMonth()]; // gets month depending on date
-    let year = today.getFullYear() // gets year
     const currentDay = $("#currentDay") // current day container
-
-    currentDay.text(`Today is ${dayOfWeek}, ${month} ${date}, ${year}`);
+    currentDay.text(moment().format('dddd, MMMM Do YYYY, h:mm:ss a'));
 }
 
 dateAndTime(); // calls function
@@ -53,9 +39,8 @@ const timeBlocks = () => {
 timeBlocks(); // calls function
 
 // Add Entries
-
 $(".saveBtn").click(function() {
-
+    let workHourClicked = this.val()
 })
 
 // Saves to Local Storage
