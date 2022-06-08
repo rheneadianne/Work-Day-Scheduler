401ddb6e479bd2e56e3e15e1ab6d3eb17ce2e65e
rhenea <rheneadianne@gmail.com>
2022-06-07 23:20:53 -0400

    added comments, changed i variable to help me think better

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,29 +22,40 @@ const dateAndTime = () => {
     currentDay.text(`Today is ${dayOfWeek}, ${month} ${date}, ${year}`);
 }
 
-dateAndTime();
+dateAndTime(); // calls function
 
-let currentHour = today.getHours()
+
+// Creating Time Blocks
+let currentHour = today.getHours() // gets current hour
 
 const timeBlocks = () => {
     const allHours = ["12 AM","1 AM","2 AM","3 AM","4 AM","5 AM","6 AM","7 AM","8 AM","9 AM","10 AM","11 AM","12 PM",
-    "1 PM","2 PM","3 PM","4 PM","5 PM","6 PM","7 PM","8 PM","9 PM","10 PM","11 PM"]
-    let workStart = 9
-    let workEnd = 17
-    const timeBlockDiv = $(".container");
+    "1 PM","2 PM","3 PM","4 PM","5 PM","6 PM","7 PM","8 PM","9 PM","10 PM","11 PM"] // all of the hours as string in array
+    let workStart = 9 // starts at 9 am
+    let workEnd = 17 // ends at 5 pm
+    const timeBlockDiv = $(".container"); // time block div
 
-    for (i = workStart; i <= workEnd; i++) {
-        //builds time blocks
-        let blocks = '<div class="row time-block"> ' + '<div class="col-md-1 hour">' + allHours[i] + '</div> ';
+    for (workHour = workStart; workHour <= workEnd; i++) { //builds time blocks for each work hour
+        let blocks = '<div class="row time-block"> ' + '<div class="col-md-1 col-2 hour">' + allHours[i] + '</div> '; // basic time block
         
-        i < currentHour ?
-            blocks = blocks + '<textarea class="col-md-10 description past" id="text' + allHours[i] + '"></textarea>' :
-        i === currentHour ?
-            blocks = blocks + '<textarea class="col-md-10 description present" id="text' + allHours[i] + '"></textarea>' :
-        blocks = blocks + '<textarea class="col-md-10 description future" id="text' + allHours[i] + '"></textarea> ';
+        workHour < currentHour ? // if time in the past
+            blocks = blocks + '<textarea class="col-md-10 col-8 description past" id="text' + allHours[i] + '"></textarea>' :
+        workHour === currentHour ? // if current hour
+            blocks = blocks + '<textarea class="col-md-10 col-8 description present" id="text' + allHours[i] + '"></textarea>' :
+        blocks = blocks + '<textarea class="col-md-10 col-8 description future" id="text' + allHours[i] + '"></textarea> '; // if time in present
         
-        timeBlockDiv.append(blocks)
+        blocks = blocks + '<button class="btn saveBtn col-md-1 col-2" value="' + allHours[i] + '"><i class="fas fa-save"></i></button>' + '</div>' // adds save button
+
+        timeBlockDiv.append(blocks) // appends blocks to container div
     }
 }
 
-timeBlocks()
+timeBlocks(); // calls function
+
+// Add Entries
+
+$(".saveBtn").click(function() {
+
+})
+
+// Saves to Local Storage
