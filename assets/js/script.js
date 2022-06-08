ae737d173ff1ef90362a1eb2b95e449d321cd4a2
rhenea <rheneadianne@gmail.com>
2022-06-07 22:18:51 -0400

    added current date <p>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,27 @@
-const currentTime = () => {
-    let today = new Date();
-    let date = today.getDate()
-    let daySuffix = "th"
+let today = new Date();
+let daySuffix = ""
+let date = today.getDate() + daySuffix
+const currentDay = $("#currentDay")
 
-    console.log(today)
+const dateAndTime = () => {
+
+    date === 1 || date === 21 || date === 31?
+        daySuffix = "st":
+    date === 2 || date === 22?
+        daySuffix = "nd":
+    date === 3 || date === 23?
+        daySuffix = "rd":
+    daySuffix = "th";
+
+    const weekdayList = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let dayOfWeek = weekdayList[today.getDay()];
+    const monthList = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let month = monthList[today.getMonth()];
+    
+    currentDay.text(`${dayOfWeek}, ${month} ${date}`);
 }
 
-currentTime()
+let currentHour = today.getHours()
+console.log(currentHour);
+
+dateAndTime()
